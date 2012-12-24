e157531c3ddac27a305028b489ca7c3ee77b1772
Tikhon Jelvis <tikhon@jelv.is>
2012-12-23 20:34:44 -0800

    Got Distr.hs to compile.

--- src/Language/Synthesis/Distr.hs
+++ src/Language/Synthesis/Distr.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
+module Language.Synthesis.Distr where
+
+import           Control.Monad        (join, replicateM)
+import           Control.Monad.Random (RandomGen, Rand, getRandom, getRandomR)
 
 data Distr a = Distr {
-    sample :: RandomGen g => Rand g a,
+    sample         :: forall g. RandomGen g => Rand g a,
     logProbability :: a -> Double
 }
 
@@ -11,16 +16,14 @@ sumByLogs :: [Double] -> Double
 sumByLogs xs = log (sum [exp (x - high) | x <- xs]) + high
     where high = maximum xs
 
-sampleCategorical' :: RandomGen g => [(a, Double)] -> Double -> Rand g a
-sampleCategorical' [(x, _)] _ = x
-sampleCategorical' ((x, weight):rest) total = do
-    acceptance :: Double <- getRandom
-    if weight/total >= acceptance
-       then return x
-       else samp rest (total - weight)
-
 sampleCategorical :: RandomGen g => [(a, Double)] -> Rand g a
-sampleCategorical items = sampleCategorical' items (sum (map snd items))
+sampleCategorical items = go items . sum $ map snd items
+  where go [(x, _)] _ = return x
+        go ((x, weight):rest) total = do
+          acceptance <- getRandom
+          if weight/total >= acceptance
+            then return x
+            else go rest $ total - weight
 
 categorical :: Eq a => [(a, Double)] -> Distr a
 categorical items = Distr (sampleCategorical items) logProb
@@ -33,17 +36,16 @@ uniform :: Eq a => [a] -> Distr a
 uniform xs = categorical [(x, 1.0) | x <- xs]
 
 randInt :: (Int, Int) -> Distr Int
-randInt range@(low, high) = Distr (randomR range) logProb
+randInt range@(low, high) = Distr (getRandomR range) logProb
     where logProb item = if low <= item && item <= high
                             then -log (fromIntegral (high - low + 1))
                             else negativeInfinity
 
 replicate :: Int -> Distr a -> Distr [a]
-replicate n orig = Distr (replicateM n orig) logProb
+replicate n orig = Distr (replicateM n $ sample orig) logProb
     where logProb xs = sum [logProbability orig x | x <- xs]
 
-
 mix :: [(Distr a, Double)] -> Distr a
-mix distrs = Distr (join (sampleCategorical distrs)) logProb
-    where logProb x = sumByLogs [log prob + logProbability distr x | 
-                                 (distr, prob) <- distrs]
+mix distrs = Distr (sampleCategorical distrs >>= sample) logProb
+    where logProb x = sumByLogs [log prob + logProbability distr x |
+                                 (distr, prob) <- distrs] 
